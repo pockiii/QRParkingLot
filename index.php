e780a808f094798cd1d03c2eb7869a07336655d5
Stefan Chao <Stefan.chao@kcl.ac.uk>
2019-12-04 15:26:44 +0000

    tidied

--- index.php
+++ index.php
@@ -6,23 +6,23 @@
 <html>
 <head>
   <link rel="stylesheet" href="style.css">
-  <script type="text/javascript" src="grid.js"></script>
-  <script type="text/javascript" src="version.js"></script>
-  <script type="text/javascript" src="detector.js"></script>
-  <script type="text/javascript" src="formatinf.js"></script>
-  <script type="text/javascript" src="errorlevel.js"></script>
-  <script type="text/javascript" src="bitmat.js"></script>
-  <script type="text/javascript" src="datablock.js"></script>
-  <script type="text/javascript" src="bmparser.js"></script>
-  <script type="text/javascript" src="datamask.js"></script>
-  <script type="text/javascript" src="rsdecoder.js"></script>
-  <script type="text/javascript" src="gf256poly.js"></script>
-  <script type="text/javascript" src="gf256.js"></script>
-  <script type="text/javascript" src="decoder.js"></script>
-  <script type="text/javascript" src="qrcode.js"></script>
-  <script type="text/javascript" src="findpat.js"></script>
-  <script type="text/javascript" src="alignpat.js"></script>
-  <script type="text/javascript" src="databr.js"></script>
+  <script type="text/javascript" src="jsqr/grid.js"></script>
+  <script type="text/javascript" src="jsqr/version.js"></script>
+  <script type="text/javascript" src="jsqr/detector.js"></script>
+  <script type="text/javascript" src="jsqr/formatinf.js"></script>
+  <script type="text/javascript" src="jsqr/errorlevel.js"></script>
+  <script type="text/javascript" src="jsqr/bitmat.js"></script>
+  <script type="text/javascript" src="jsqr/datablock.js"></script>
+  <script type="text/javascript" src="jsqr/bmparser.js"></script>
+  <script type="text/javascript" src="jsqr/datamask.js"></script>
+  <script type="text/javascript" src="jsqr/rsdecoder.js"></script>
+  <script type="text/javascript" src="jsqr/gf256poly.js"></script>
+  <script type="text/javascript" src="jsqr/gf256.js"></script>
+  <script type="text/javascript" src="jsqr/decoder.js"></script>
+  <script type="text/javascript" src="jsqr/qrcode.js"></script>
+  <script type="text/javascript" src="jsqr/findpat.js"></script>
+  <script type="text/javascript" src="jsqr/alignpat.js"></script>
+  <script type="text/javascript" src="jsqr/databr.js"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
   <script> //disable reload
